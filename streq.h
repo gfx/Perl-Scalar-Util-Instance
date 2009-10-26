f526b172e7b5f983ede8003bb0110e717b7c70ac
Goro Fuji <gfuji@cpan.org>
2009-10-26 13:46:11 +0900

    Tweaks for streq.h

--- streq.h
+++ streq.h
@@ -1,39 +1,48 @@
+/*
+ * streq.h - Check string equality
+ *
+ * Provides: strEQ(a, b), strnEQ(a, b, n), strNE(a, b), strnNE(a, b, n)
+ *
+ */
+
 #ifndef INLINED_STR_EQ_H
 #define INLINED_STR_EQ_H
-
-#ifdef INLINE_STR_EQ
 
 #if (!defined(__cplusplus__) || !defined(__STDC_VERSION__) ||  (__STDC_VERSION__ < 199901L)) && !defined(inline)
 #define inline
 #endif
 
-
 #undef strnEQ
 static inline int
 strnEQ(const char* const x, const char* const y, size_t const n){
-    size_t i;
+    register size_t i;
     for(i = 0; i < n; i++){
         if(x[i] != y[i]){
-            return FALSE;
+            return 0;
         }
     }
-    return TRUE;
+    return 1;
 }
+
 #undef strEQ
 static inline int
 strEQ(const char* const x, const char* const y){
-    size_t i;
+    register size_t i;
     for(i = 0; ; i++){
         if(x[i] != y[i]){
-            return FALSE;
+            return 0;
         }
         else if(x[i] == '\0'){
-            return TRUE; /* y[i] is also '\0' */
+            return 1; /* y[i] is also '\0' */
         }
     }
-    return TRUE; /* not reached */
+    return 1; /* not reached */
 }
 
-#endif /* !INLINE_STR_EQ */
+#undef  strnNE
+#define strnNE(a, b, n) (!strnEQ(a, b, n))
 
-#endif /* include guard */
+#undef  strNE
+#define strNE(a, b) (!strEQ(a, b))
+
+#endif /* !INLINED_STR_EQ_H */
